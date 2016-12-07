d137cd65cf1f638a90a138cd61a0f248e1d1f151
ucesres <ed.sharp@ucl.ac.uk>
2016-12-07 15:46:54 +0000

    auto update

--- code/launcher_daily.sh
+++ code/launcher_daily.sh
@@ -6,8 +6,7 @@ cd home/pi/projects/Air_po_pi/code
 sudo python daily_tasks.py
 cd /
 
-cd home/pi/projects/Air_po_pi/code
-#sudo chown -R pi:pi /home/pi/projects/web_pi/
+cd home/pi/projects/Air_po_pi
 git add .
 git commit -m "auto update"
 git push https://ucesres:cambodia06@github.com/ucesres/air_po_pi.git 
